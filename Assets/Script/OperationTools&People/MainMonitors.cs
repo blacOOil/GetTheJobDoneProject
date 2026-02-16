1337cc05a3893525737ff56c81684d8a4aeb5913
blacOOil <111561598+blacOOil@users.noreply.github.com>
2026-02-16 23:46:15 +0700

    AddMainDashBoard

---
+++ Assets/Script/OperationTools&People/MainMonitors.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class MainMonitors : MonoBehaviour
+{
+    public int totalThreatNum, totalOperation, totalContainment, TotalThreatMean, Operationrate, Containmentrate,Resouresusage,Maxresoures;
+    public TextMeshProUGUI totalThreatText, totalOperationText, totalContainmentText, TotalThreatMeanText, OperationrateText, ContainmentrateText, ResouresusageText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
